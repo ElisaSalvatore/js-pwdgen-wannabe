003056ace2c1c5efc8802ad566bff7d198608e8e
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-02 15:59:40 +0100

    user data script added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,25 @@
 // PROMPT
 
 // Chiedere nome utente - CONST
+const userName = prompt('Inserisci il tuo nome:');
+console.log(userName);
 
 // Chiedere cognome utente - CONST
+const userSurname = prompt('Inserisci il tuo cognome:')
+console.log(userSurname);
 
 // Chiedere colore preferito - CONST
+const userColor = prompt('Inserisci il tuo colore preferito:')
+console.log(userColor);
 
 // result: Password generata - LET
+let userData = userName + userSurname + userColor
+console.log(userData);
 
-// result + anno corrente - LET
+// // Anno corrente - LET
+let currentYear = 21;
+console.log(currentYear);
+
+// result + anno - LET
+let htmlElement = document.getElementById('userPwd');
+htmlElement.innerHTML = `${userData} ${currentYear}`
