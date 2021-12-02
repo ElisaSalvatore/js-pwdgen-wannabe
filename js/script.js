e3c86d4a0b9aacf773c6039367fe4312c5c7b798
ElisaSalvatore <elisa.salvatore110@gmail.it>
2021-12-02 15:32:57 +0100

    user data script structure

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// PROMPT
+
+// Chiedere nome utente - CONST
+
+// Chiedere cognome utente - CONST
+
+// Chiedere colore preferito - CONST
+
+// result: Password generata - LET
+
+// result + anno corrente - LET
